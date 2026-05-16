fb2c6de1fe7581144f03d5e8d9902909fc4ea9c9
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-05-16 21:38:31 +0300

    add flake8 file and fixes files using flake8

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-from django.db.models.fields import return_None
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,6 +5,7 @@ from django.http import HttpResponse
 # Контроллер GET запроса и рендеринга страницы home.html
 def view_home(request):
     return render(request, 'catalog/home.html')
+
 
 # Контроллер POST запроса получения обратной связи со страницы и рендеринга страницы contact.html
 # И контроллер GET запроса и рендеринга страницы contact.html если не POST запрос
@@ -15,7 +15,6 @@ def contact(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        status = request.SATUS
         # Возвращаем простой ответ
         print(f"Спасибо, {name}! Ваше телефон {phone} и сообщение {message} получены.")
         return HttpResponse(f"Спасибо, {name}! Ваше телефон {phone} и сообщение {message} получены.")
