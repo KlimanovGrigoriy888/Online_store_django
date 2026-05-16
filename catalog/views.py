3c6bc18cbc8e1857d02353715fa11978348d2949
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-05-16 21:29:13 +0300

    add home.html and contacts.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
+from django.db.models.fields import return_None
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Контроллер GET запроса и рендеринга страницы home.html
+def view_home(request):
+    return render(request, 'catalog/home.html')
+
+# Контроллер POST запроса получения обратной связи со страницы и рендеринга страницы contact.html
+# И контроллер GET запроса и рендеринга страницы contact.html если не POST запрос
+def contact(request):
+    if request.method == 'POST':
+        # Если метод запроса сервера POST получаем данные с web страницы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        status = request.SATUS
+        # Возвращаем простой ответ
+        print(f"Спасибо, {name}! Ваше телефон {phone} и сообщение {message} получены.")
+        return HttpResponse(f"Спасибо, {name}! Ваше телефон {phone} и сообщение {message} получены.")
+    return render(request, 'catalog/contacts.html')
