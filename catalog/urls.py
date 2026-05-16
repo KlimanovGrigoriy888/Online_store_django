6a2b2dc5c95dc4e90fa6380a5677212dad343069
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-05-16 21:30:44 +0300

    add urls.py in config/ and catalog/

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# Создаем пространство имен с названием catalog/ для страниц contacts.html и home.html, не забыть включить в путь в
+# директории config/urls.py
+app_name = 'catalog'
+
+# Создаем маршрутизацию пути html страниц и контроллеров
+urlpatterns = [
+    path('home/', views.view_home, name='home'),
+    path('contacts/', views.contact, name='contacts')
+]
