32275db6757879382dfab5ea9b1442c066c0cee3
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-05-18 20:07:17 +0300

    fixes call method dir app_name

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from catalog.apps import CatalogConfig
 
 # Создаем пространство имен с названием catalog/ для страниц contacts.html и home.html, не забыть включить в путь в
 # директории config/urls.py
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 # Создаем маршрутизацию пути html страниц и контроллеров
 urlpatterns = [
