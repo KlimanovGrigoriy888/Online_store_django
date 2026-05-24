693de48e62757b9e082a280b79d0c22fd1e42fc3
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-05-24 20:20:07 +0300

    add catalog/models.py with new class Category and Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Класс создания категории продукта"""
+
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+
+    class Meta:
+        verbose_name = 'категория продукта'
+        verbose_name_plural = 'категории продуктов'
+        ordering = ['name',]
+
+
+class Product(models.Model):
+    """Класс создания продукта"""
+
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    picture = models.ImageField(upload_to='photos/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', related_name='product')
+    purchase_price = models.IntegerField(default=0, verbose_name='Цена за покупку' )
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.category}'
+
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name',]
