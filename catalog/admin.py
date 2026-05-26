8e4866b2cc82c2ed15dc442f91675ae0f68eec1b
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-05-26 22:31:29 +0300

    add new settings for class Contact

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Contact
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,6 +11,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'purchase_price', 'category',)
+    list_display = ('id', 'name', 'purchase_price', 'category', 'created_at',)
     list_filter = ('category', )
     search_fields = ('name', 'description',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'created_at',)
+    list_filter = ('first_name', 'last_name',)
+    search_fields = ('first_name', 'last_name', 'email')
