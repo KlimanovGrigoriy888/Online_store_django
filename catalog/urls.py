5e43c814266cacc955504861bfcb0ca4cca55ba1
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-06-01 20:38:48 +0300

    add route for product_detail.html and product_input.html to enter product data

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from mypy.types import names
+
 from . import views
 from catalog.apps import CatalogConfig
 
@@ -9,5 +11,7 @@ app_name = CatalogConfig.name
 # Создаем маршрутизацию пути html страниц и контроллеров
 urlpatterns = [
     path('home/', views.view_home, name='home'),
-    path('contacts/', views.contact, name='contacts')
+    path('contacts/', views.contact, name='contacts'),
+    path('product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('product_input', views.product_input_form, name='product_input'),
 ]
