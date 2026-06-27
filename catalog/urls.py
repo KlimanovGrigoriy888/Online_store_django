d5822508818f2cf3cdd1cf4d7a05535a4c8b9397
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-06-27 22:32:26 +0300

    changed the new connection for the catalog controller

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-
-from . import views
 from catalog.apps import CatalogConfig
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
+    ContactView
 
-# Создаем пространство имен с названием catalog/ для страниц contacts.html и home.html, не забыть включить в путь в
-# директории config/urls.py. Для этого используем класс CatalogConfig для вызова имени директории в catalog.apps
+# Создаем пространство имен с названием catalog/ для страниц contacts.html и products.html и остальных, не забыть
+# включить в путь в директории config/urls.py. Для этого используем класс CatalogConfig для вызова имени директории
+# в catalog.apps
 app_name = CatalogConfig.name
 
 # Создаем маршрутизацию пути html страниц и контроллеров
 urlpatterns = [
-    path('home/', views.view_home, name='home'),
-    path('contacts/', views.contact, name='contacts'),
-    path('product_detail/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('product_input/', views.product_input_form, name='product_input'),
+    path('products/', ProductListView.as_view(), name='products_list'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
