805ec30d891ac55813df2b3e03b0a92208717a5c
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-07-26 16:49:19 +0300

    Fixed changes: Added LoginReguiredMixin and PermissionsReguiredMixin to the blog model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.mail import send_mail  # Импортируем функцию отправки почты
@@ -48,7 +49,7 @@ class BlogDetailView(DetailView):
         return self.object
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Класс создания блоговой записи."""
     model = BlogEntry
     # Указываем поля модели, которые будут в HTML-форме
@@ -57,15 +58,19 @@ class BlogCreateView(CreateView):
     template_name = 'blog/blog_form.html'
     # Перенаправляем пользователя после успешного создания товара
     success_url = reverse_lazy('blog:blog_list')
+    # Добавление ограничения для пользователей
+    permission_required = 'blog.add_blogentry'
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Класс изменения блоговой записи."""
     model = BlogEntry
     # Указываем поля модели, которые будут в HTML-форме
     fields = ['title', 'content', 'preview', 'is_published']
     # Новая страница с формой
     template_name = 'blog/blog_form.html'
+    # Добавление ограничения для пользователей
+    permission_required = 'blog.change_blogentry'
     # # Перенаправляем пользователя после успешного создания товара не работает, необходимо применить
     # # get_success_url(self):
     # success_url = reverse_lazy('blog:blog_detail')
@@ -76,10 +81,12 @@ class BlogUpdateView(UpdateView):
         return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Класс просмотра удаления блоговой записи."""
     model = BlogEntry
     # Новая страница с формой
     template_name = 'blog/blog_confirm_delete.html'
     # Перенаправляем пользователя после успешного удаления товара
     success_url = reverse_lazy('blog:blog_list')
+    # Добавление ограничения для пользователей
+    permission_required = 'blog.delete_blogentry'
