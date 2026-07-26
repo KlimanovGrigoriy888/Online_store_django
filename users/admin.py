9fd0d2a1fe40f9d0d5532a6149247684abc4c33b
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-07-26 16:46:48 +0300

    Addition of an admin registration form

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class AuthorAdmin(admin.ModelAdmin):
+    # Пишем exclude и указываем в нем только то, что не хотим видеть в админке, т.е. остальные поля будут видны
+    exclude = ('password',)
