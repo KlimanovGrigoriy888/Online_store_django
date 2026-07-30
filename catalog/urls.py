030526485429c956b58cd44febec2469dc888946
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-07-30 20:56:01 +0300

    add cache template for ProductDetailView and add path for view ProductsByCategoryListView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
-    ContactView
+    ContactView, ProductsByCategoryListView
 
 # Создаем пространство имен с названием catalog/ для страниц contacts.html и products.html и остальных, не забыть
 # включить в путь в директории config/urls.py. Для этого используем класс CatalogConfig для вызова имени директории
@@ -12,8 +14,10 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    #  Настройка кеширования страницы на 60 секунд
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('category/<int:category_id>/', ProductsByCategoryListView.as_view(), name='products_by_category'),
 ]
